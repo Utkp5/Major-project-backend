59d169bcadffcf64c55997824a33e21ec95745ff
Utkp5 <utkp200@gmail.com>
2023-02-21 12:53:10 +0530

    frontend

--- major_project_23_frontend/src/App.js
+++ major_project_23_frontend/src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
-import Layout from './Components/Layouts/Layout/Layout';
+import React,{Suspense, lazy} from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Loading from "./Components/Layouts/Loading/Loading";
+
+
+const Home    = lazy(() => import('./Components/Pages/Home/Home'));
+const About   = lazy(() => import('./Components/Pages/About/About'));
+const Contact = lazy(() => import('./Components/Pages/Contact/Contact'));
+const Signup = lazy(() => import('./Components/Pages/Signup/Signup'));
+const Signin = lazy(() => import('./Components/Pages/Signin/Signin'));
+const Profile = lazy(() => import('./Components/Pages/Profile/Profile'));
+const Pagenotfound = lazy(() => import('./Components/Pages/Pagenotfound'));
 
 function App() {
   return (
-    <div>
-      <Layout>
-        <h1>E-comm</h1>
-      </Layout>
-    </div>
+    <Suspense fallback={<div><Loading /></div>}>
+
+      <Router>
+        <Routes>
+          <Route  exact path='/' element={<Home />}/>   
+          <Route  exact path='/About' element={<About />}/>   
+          <Route  exact path='/Contact' element={<Contact />}/>   
+          <Route  exact path='/Signup' element={<Signup />}/>   
+          <Route  exact path='/Signin' element={<Signin />}/>   
+          <Route  exact path='/Profile' element={<Profile />}/>   
+          <Route  exact path='/Pagenotfound' element={<Pagenotfound />}/>   
+        </Routes>
+      </Router>
+
+    </Suspense>        
+      
+
+         
+
   );
 }
 
