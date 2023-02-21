ca42855bc0e348c9500693fdadfeb4f7a01c4584
Utkp5 <utkp200@gmail.com>
2023-02-21 13:08:31 +0530

    frontend

--- major_project_23_frontend/src/Components/Layouts/Navbar/Navbar.js
+++ major_project_23_frontend/src/Components/Layouts/Navbar/Navbar.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 
 function Navbar() {
   return (
-    <div>
-      Navbar
+    <div className='navbar'>
+
+      <div className='b_name'></div>
+
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+      </ul>
+
+      <ul className={menu ? 'navbar_mobile_css' : 'navbar_ul'}>
+        <li> <NavLink to="/">Home</NavLink> </li>
+        <li> <NavLink to="/About">About</NavLink> </li>
+        <li><NavLink to="/Contact" id="a" >Contact</NavLink></li>
+        {!localStorage.getItem("token")? // ye command signup button ko hide krne ke liye hai.
+        <li><NavLink to="/Signup" className="signin_button"><button className="navbar_button" >Sign up</button></NavLink></li>
+        :null
+        }
+        <li><NavLink to={inoutDirection} className="signin_button"><button className="navbar_button" onClick={() => {
+        Handlechange();
+        }}>{inout}</button></NavLink></li>
+      </ul>
+
+      <div className="menu_icon" onClick={() => setmenu(!menu)}>
+        {menu ? <HiX size={26} color="white"/> :<HiMenu size={26} color="white"/>}
+      </div> 
+
+      
     </div>
   )
 }
