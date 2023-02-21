d4a91ac1d96f80dd72fa988fc4a721edf99889da
Utkp5 <utkp200@gmail.com>
2023-02-21 18:22:23 +0530

    frontend

--- major_project_23_frontend/src/Components/Layouts/Footer/Footer.js
+++ major_project_23_frontend/src/Components/Layouts/Footer/Footer.js
@@ -1,12 +1,41 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
+import { NavLink } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className='footer'>
-      footer
+    <div className="footer">
+
+      <div className="row primary">
+        <div className="column about">
+          <h3 className="fo_h3">Connect</h3>
+          <p><i className="fa fa-map-marker" aria-hidden="true"></i>Burari, Sant Nagar Delhi-84 India</p>
+          <div className="social">
+            <i className="fa fa-facebook-square fo_i" ></i>
+            <i className="fa fa-twitter-square fo_i" ></i>
+            <i className="fa fa-linkedin-square fo_i" ></i>
+            <i className="fa fa-instagram fo_i" ></i>
+          </div>
+        </div>
+
+        <div className="column link">
+          <h3 className="fo_h3">Links</h3>
+          <ul className="fo_ul">
+            <li className="fo_li"><NavLink className="fo_navlink" to="/">Home</NavLink></li>
+            <li className="fo_li"><NavLink className="fo_navlink" to="/About">About</NavLink></li>
+            <li className="fo_li"><NavLink className="fo_navlink" to="/Contact">Contact</NavLink></li>
+            <li className="fo_li"><NavLink className="fo_navlink" to="/Category">Category</NavLink></li>
+          </ul>
+        </div>
+
+        <div className="column subscribe">
+          <h3 className="fo_h3">Newsletter</h3>
+          <div><input className="fo_input" type="email" placeholder="Your email id here" /><button className="fo_btn">Subscribe</button></div>
+        </div>
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- major_project_23_frontend/src/Components/Layouts/Navbar/Navbar.js
+++ major_project_23_frontend/src/Components/Layouts/Navbar/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
 
   const  [inout,setinout] = useState();
    const [menu,setmenu] = useState(false);
-   const [inoutDirection, setDirection] = useState("/Signin");
+   const [inoutDirection, setDirection] = useState("/Sign in");
     
    useEffect(() => {
     let tokenval = localStorage.getItem("token");
@@ -20,8 +20,8 @@ function Navbar() {
     }
     else
     {
-      setinout("Sigin");
-      setDirection("/Signin");
+      setinout("Sig in");
+      setDirection("/Sign in");
     }
    },[])
 
