c1f84218bcbbf85775f1a3597e25b67246b65c37
Utkp5 <utkp200@gmail.com>
2023-02-24 17:04:29 +0530

    update models

--- Models/user.js
+++ Models/user.js
@@ -27,6 +27,10 @@ const userSchema = new mongoose.Schema (
             type : String,
             require :true,
         },
+        question : {
+            type : String,
+            require : true,
+        },
         role : {
             type : Number,
             default : 0,
