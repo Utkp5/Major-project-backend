2603f94a42bd53a88f3ae90593dca7eefb07f77e
Utkp5 <utkp200@gmail.com>
2023-02-28 19:20:32 +0530

    frontend

--- major_project_23_frontend/src/Components/Pages/Admin/Crtcategory/Createcategory.js
+++ major_project_23_frontend/src/Components/Pages/Admin/Crtcategory/Createcategory.js
@@ -4,6 +4,7 @@ import { toast } from "react-hot-toast";
 import Adminmenu from "../../../Layouts/Adminmenu/Adminmenu";
 import Layout from "../../../Layouts/Layout/Layout";
 import "../Admindashboard.css";
+import "./Createcategory.css";
 
 function Createcategory() {
   const [categories, setcategories] = useState([]);
@@ -39,18 +40,18 @@ function Createcategory() {
           <h2 className="admin_m_h2">Manage Categories</h2>
 
           <div className="admin_m_sub2">
-            <div>
+            <div id="admin_table">
               <thead>
                 <tr>
-                  <th>Name</th>
-                  <th>Action</th>
+                  <th className="admin_th">Name</th>
+                  <th className="admin_th">Action</th>
                 </tr>
               </thead>
               <tbody>
                 {categories?.map((c) => (
                   <>
                     <tr>
-                      <td key={c._id}>{c.name}</td>
+                      <td key={c._id} id="admin_td">{c.name}</td>
                       <td><button>Edit</button></td>
                     </tr>
                   </>
