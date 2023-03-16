d859452c6fda8309c3ca36f24c8669c12483cf6d
Utkp5 <utkp200@gmail.com>
2023-03-16 18:21:31 +0530

    index.js chng

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 import cors from 'cors'
 import morgan  from "morgan";
-import path  from "path";
+import path from 'path';
+const __dirname = path.resolve();
 
 
 
